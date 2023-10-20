9773fd0751ad2260e261750beeb2ca036d72343c
kohsaka <pe6_7@yahoo.co.jp>
2023-10-20 19:48:40 +0900

    first

---
+++ src/main/java/jp/kuskyst/aozorapdf/controller/HelloController.java
@@ -0,0 +1,23 @@
+package jp.kuskyst.aozorapdf.controller;
+
+import jp.kuskyst.aozorapdf.service.HelloService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    // @Autowired
+    private final HelloService helloService;
+
+    public HelloController(HelloService helloService) {
+        this.helloService = helloService;
+    }
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return this.helloService.hello(name);
+    }
+
+}
