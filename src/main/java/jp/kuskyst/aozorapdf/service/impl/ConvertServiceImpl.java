d5fed07529752a5153c9b7e98fd12cdc60f2a02a
kohsaka <pe6_7@yahoo.co.jp>
2023-10-22 12:44:52 +0900

    sunday

--- src/main/java/jp/kuskyst/aozorapdf/service/impl/ConvertServiceImpl.java
+++ src/main/java/jp/kuskyst/aozorapdf/service/impl/ConvertServiceImpl.java
@@ -5,6 +5,9 @@ import jp.kuskyst.aozorapdf.entity.ConvertParam;
 import jp.kuskyst.aozorapdf.service.ConvertService;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.common.PDRectangle;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,14 +16,28 @@ public class ConvertServiceImpl implements ConvertService {
     @Override
     public PDDocument convert(ConvertParam param) {
         try {
-            PDDocument res = new PDDocument();
+            PDDocument document = new PDDocument();
+            PDPage page = new PDPage(PDRectangle.A4);
+            document.addPage(page);
 
-            PDPage page = new PDPage();
-            res.addPage(page);
+            PDType1Font font = PDType1Font.HELVETICA;
+            PDPageContentStream contentStream = new PDPageContentStream(document, page);
+            contentStream.setFont(font, 12);
 
-            res.save(param.getName() + ".pdf");
-            res.close();
-            return res;
+            String text = param.getText();
+            float x = 100, y = 700, lineHeight = 14;
+            for (int i = text.length() - 1; i >= 0; i--) {
+                String character = text.substring(i, i + 1);
+                contentStream.beginText();
+                contentStream.newLineAtOffset(x, y - (text.length() - 1 - i) * lineHeight);
+                contentStream.showText(character);
+                contentStream.endText();
+            }
+
+            contentStream.close();
+            document.save(param.getName()+".pdf");
+            document.close();
+            return document;
         } catch (IOException e) {
             return null;
         }
